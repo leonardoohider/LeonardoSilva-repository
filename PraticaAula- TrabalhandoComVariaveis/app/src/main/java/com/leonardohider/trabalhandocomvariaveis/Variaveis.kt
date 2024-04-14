542b45ca64825e853d818c5cee242eb05c6fb01f
leonardoohider <163493141+leonardoohider@users.noreply.github.com>
2024-04-14 02:40:59 -0300

    1111
    11111

---
+++ PraticaAula- TrabalhandoComVariaveis/app/src/main/java/com/leonardohider/trabalhandocomvariaveis/Variaveis.kt
@@ -0,0 +1,12 @@
+package com.leonardohider.trabalhandocomvariaveis
+
+class Variaveis {
+}
+// Declaraçao de variaveis mutaveis e imutaveis
+
+fun main() {
+    var resultado: Int
+    var inteiro = 5
+
+    val inteiro2 = 2
+}
